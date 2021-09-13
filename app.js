9a21eba8f08c468286f55fd658f8b9f1aa58dd68
QuangDo <qdangdo@gmail.com>
2021-09-13 16:08:08 -0500

    Fixed Fetch

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ console.log(date)
 
 
 function renderInputs(origin, destination, departingDate, returnDate = '') {
+    let returnDateUrl = ''
     if (returnDate) {
-        const returnDateUrl = `?inboundpartialdate=${returnDate}`
-    }
-    else {
-        const returnDateUrl = ''
+        returnDateUrl = `?inboundpartialdate=${returnDate}`
     }
     const URL = `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/US/USD/en-US/${origin}/${destination}/${departingDate}/` + returnDateUrl
     console.log(URL)
@@ -20,10 +18,8 @@ function renderInputs(origin, destination, departingDate, returnDate = '') {
 		"x-rapidapi-key": "4c29bc4fdemsh7a37d6b25c8c2f1p1a2bb7jsn876b1a4c77b9"
 	}
     })
-    .then(response => {response.json()})
-    .then(data => data.forEach(element, {
-        
-    }))
+    .then(response => response.json())
+    .then(data => console.log(data))
     .catch(err => {
 	console.error(err);
     });
